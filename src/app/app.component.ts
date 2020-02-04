9889070b9866011ca5ca51b9e5d385ce08de930e
jch2617 <jch2617@gmail.com>
2020-02-04 10:11:16 -0500

    fixed link and thumbnail now appears

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RedditPostDetailComponent } from './reddit-post-detail/reddit-post-detail.component';
 
@@ -13,7 +12,6 @@ import { RedditPostDetailComponent } from './reddit-post-detail/reddit-post-deta
   ],
   imports: [
     BrowserModule,
-   // AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
